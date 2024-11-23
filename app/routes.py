82b6a122fe5acfe34d01396c37ff7b0f78fe4481
Sean McAuliffe <seanmcauliffe4@gmail.com>
2024-11-23 15:39:17 -0500

    routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,41 @@
-import os
-from flask import Blueprint, render_template, redirect, url_for, jsonify, request
+"""
+This module defines the main blueprint for the web application, which includes
+routes for listing players, viewing player details, displaying a dashboard,
+and serving player statistics data via an API endpoint.
+
+Blueprint Name:
+- main: The main blueprint for rendering player and dashboard views.
+
+Endpoints:
+- `/`: Displays a list of all players.
+- `/player/<int:player_id>`: Displays detailed statistics for a specific
+player.
+- `/dashboard`: Renders the dashboard page for player statistics.
+- `/api/dashboard`: Serves JSON data of player statistics for use in the
+dashboard.
+"""
+
+from flask import (
+    Blueprint,
+    render_template,
+    redirect,
+    url_for,
+    jsonify,
+    request
+)
+
 from app.models import Player, Statistics, LeagueDashPlayerStats
 
 main = Blueprint('main', __name__)
 
+
 @main.route('/')
 def player_list():
     """Display a list of all players."""
-    players = Player.get_all_players()  # Retrieve all players from the database
+    players = Player.get_all_players()
+    # Retrieve all players from the database
     return render_template('player_list.html', players=players)
+
 
 @main.route('/player/<int:player_id>')
 def player_detail(player_id):
@@ -20,10 +47,12 @@ def player_detail(player_id):
     stats = Statistics.get_stats_by_player(player_id)  # Retrieve player stats
     return render_template('player_detail.html', player=player, stats=stats)
 
+
 @main.route('/dashboard')
 def dashboard():
     """Render the dashboard with player stats."""
     return render_template('dashboard.html')
+
 
 @main.route('/api/dashboard')
 def dashboard_data():
