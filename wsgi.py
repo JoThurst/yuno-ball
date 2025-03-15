1d5d1eeac3c7c166a06a4de85236b6d39f550e73
Jordan Thurston <jothurst@gmail.com>
2025-03-15 14:14:07 -0400

    Deployment configuration fixes

--- wsgi.py
+++ wsgi.py
@@ -2,15 +2,21 @@ import os
 import sys
 
 # Add the application directory to the Python path
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
 # Set environment variables for proxy configuration
-os.environ["PROXY_ENABLED"] = "true"
-os.environ["FORCE_PROXY"] = "true"
+os.environ['PROXY_ENABLED'] = 'true'
+os.environ['FORCE_PROXY'] = 'true'
+os.environ['FORCE_LOCAL'] = 'false'
 
-# Import the app
 from app import create_app
-application = create_app()
+from asgiref.wsgi import WsgiToAsgi
 
-# For Gunicorn
-app = application 
+# Create the Flask application
+flask_app = create_app()
+
+# Wrap the Flask app with ASGI adapter
+app = WsgiToAsgi(flask_app)
+
+# Make the app available for both WSGI (gunicorn) and ASGI (uvicorn)
+application = app 
