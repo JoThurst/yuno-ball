5c346784c2dfe620baab07dbf7f409194c1a3ea9
Jordan Thurston <jothurst@gmail.com>
2025-03-19 00:18:03 -0400

    Deployment fixes for CORS with nonce welcome page enhancement 1.0

--- app/utils/security_config.py
+++ app/utils/security_config.py
@@ -35,7 +35,7 @@ PROD_SECURITY_HEADERS = {
     'Permissions-Policy': 'camera=(), microphone=(), geolocation=()',
     'Content-Security-Policy': "\
         default-src 'self'; \
-        script-src 'strict-dynamic' 'nonce-{nonce}' https: http:; \
+        script-src 'self' 'unsafe-inline' 'nonce-{nonce}'; \
         style-src 'self' 'unsafe-inline'; \
         img-src 'self' data: stats.nba.com *.nba.com; \
         font-src 'self'; \
